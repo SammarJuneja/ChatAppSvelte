d9a0ca2cf105e8d23030bee575c08ae4d343d0e9
SammarJuneja <sammarkumar345@gmail.com>
2024-05-28 15:51:00 +0530

    Created welcome page

---
+++ src/lib/+Login.svelte
@@ -0,0 +1,15 @@
+<script>
+
+</script>
+
+<div class="bg-gray-900 grid h-96">
+    <h1 class="text-white text-2xl flex items-center justify-center mt-10">Welcome to the Chat app</h1>
+    <div class="mt-20 border-2 mb-10 p-10 rounded-2xl border-gray-950 bg-gray-950 text-white text-lg mx-auto">
+        <button class="bg-blue-700 my-2 py-1 flex items-center justify-center w-40 rounded-md">
+            <h3>Signup</h3>
+        </button>
+        <button class="bg-neutral-700 my-2 py-1 flex items-center justify-center w-40 rounded-md">
+            <h3>Login</h3>
+        </button>
+    </div>
+</div>
