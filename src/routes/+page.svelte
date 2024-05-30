5df0a1fb0b73436e7a83b3ad070ab159a0e49f38
SammarJuneja <sammarkumar345@gmail.com>
2024-05-30 16:57:59 +0530

    Made login page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,10 @@
 <script lang="ts">
 import "../app.css";
-import Login from "../lib/+Login.svelte";
-import Signup from "../lib/+Signup.svelte";
 </script>
 
 
-<!-- <h1>Welcome to the chat app</h1> -->
-
-<div class="bg-gray-900 grid h-full pb-96">
+<!-- Routing function needed -->
+<div class="grid">
     <h1 class="text-white text-2xl flex items-center justify-center mt-20">Welcome to the Chat app</h1>
     <div class="mt-40 border-2 mb-10 p-10 rounded-2xl border-gray-950 bg-gray-950 text-white text-lg mx-auto hover:p-12">
         <button class="bg-blue-700 my-2 py-1 flex items-center justify-center w-40 rounded-md hover:p-2">
---
+++ src/routes/logging/Login.svelte
@@ -0,0 +1,13 @@
+<script>
+
+</script>
+
+<div class="grid items-center justify-center">
+    <div class="bg-slate-600 grid items-center mt-60 p-16 rounded-lg w-60 justify-center">
+        <input class="bg-neutral-800 rounded-lg text-white outline-none p-3 hover:border-2 hover:border-slate-400 hover:transition-all" placeholder="Email" type="text">
+        <input class="bg-neutral-800 rounded-lg text-white outline-none p-3 hover:border-2 hover:border-slate-400 hover:transition-all mt-5" placeholder="Password" type="password">
+    </div>
+    <div class="flex items-center justify-center">
+        <button class="p-2 mt-5 w-40 bg-zinc-800 text-white rounded-lg hover:w-full hover:transition-all">Login</button>
+    </div>
+</div>
